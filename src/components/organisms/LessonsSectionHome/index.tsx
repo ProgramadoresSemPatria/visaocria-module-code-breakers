779f7f5f37ce755729fb90363f55ea663d0ce285
Gabriel Melo <bielrj170@gmail.com>
2024-11-27 21:07:12 -0300

    feat(component): create a lessons section organism for homepage

---
+++ src/components/organisms/LessonsSectionHome/index.tsx
@@ -0,0 +1,18 @@
+import Title from '@/components/atoms/Title';
+import LessonsList from '@/components/molecules/LessonsList';
+
+const LessonsSectionHome = () => {
+  return (
+    <section className="flex flex-col gap-20">
+      <Title
+        level={3}
+        className="text-[#e0e0e0] text-center text-[56px] font-bold"
+      >
+        Lessons
+      </Title>
+      <LessonsList />
+    </section>
+  );
+};
+
+export default LessonsSectionHome;
