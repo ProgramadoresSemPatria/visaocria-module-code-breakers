636ffd3d89c810f217cea7198d0517f62d054650
Gabriel Melo <bielrj170@gmail.com>
2024-11-27 21:19:09 -0300

    feat(page): create a roadmap page

---
+++ src/components/UIPages/RoadmapPage/index.tsx
@@ -0,0 +1,13 @@
+import Diagram from '@/components/organisms/Diagram';
+
+const RoadmapPage = () => {
+  return (
+    <>
+      <div className="flex flex-row">
+        <Diagram />
+      </div>
+    </>
+  );
+};
+
+export default RoadmapPage;
