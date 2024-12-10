057dcfedc20f67db1703d3c652a3c9ca2e4ba2ea
Gabriel Melo <bielrj170@gmail.com>
2024-12-09 23:56:54 -0300

    style: fix lint error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- frontend/src/app/courses/page.tsx
+++ frontend/src/app/courses/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import CoursesPage from 'frontend/src/components/UIPages/CoursesPage';
+import CoursesPage from 'src/components/UIPages/CoursesPage';
 
 export default function Courses() {
   return (
