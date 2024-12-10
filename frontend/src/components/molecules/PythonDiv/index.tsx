1e99925eaecaf0d6e8dc2e70c28a023b0dd9e23c
Gabriel Melo <bielrj170@gmail.com>
2024-12-09 23:48:48 -0300

    refactor: change mocked data to data from api

--- frontend/src/components/molecules/PythonDiv/index.tsx
+++ frontend/src/components/molecules/PythonDiv/index.tsx
@@ -1,35 +1,21 @@
-import Subheading from 'frontend/src/components/atoms/Subheading';
-import Title from 'frontend/src/components/atoms/Title';
+import Subheading from 'src/components/atoms/Subheading';
+import Title from 'src/components/atoms/Title';
 import CoursesList from '../CoursesList';
-
-const SystemDesignCourses = [
-  {
-    url: 'python-for-beginners',
-    title: 'Python for Beginners',
-    description: 'Learn the python programming language.',
-    duration: '12 hours',
-    difficulty: 'Easy',
-    imageUrl: '/images/logoBgTransparent.webp',
-  },
-  {
-    url: 'python-for-coding-interviews',
-    title: 'Python for Coding Interviews',
-    description: 'Learn effective Python for coding interviews.',
-    duration: '8 hours',
-    difficulty: 'Easy',
-    imageUrl: '/images/logoBgTransparent.webp',
-  },
-  {
-    url: 'python-oop',
-    title: 'Python OOP',
-    description: 'Learn object-oriented programming in Python.',
-    duration: '10 hours',
-    difficulty: 'Easy',
-    imageUrl: '/images/logoBgTransparent.webp',
-  },
-];
+import useCoursesData from 'src/hooks/useCoursesData';
 
 const PythonDiv = () => {
+  const { coursesData, query } = useCoursesData();
+  const PythonDivCourses = coursesData.filter(
+    (course) => course.matter === 'PYTHON',
+  );
+
+  if (query.isLoading) {
+    return <p>Carregando cursos...</p>;
+  }
+
+  if (query.isError) {
+    return <p>Erro ao carregar os cursos</p>;
+  }
   return (
     <div className="flex flex-row gap-[280px] justify-between px-52">
       <div className="flex flex-col gap-4 w-[400px]">
@@ -42,7 +28,7 @@ const PythonDiv = () => {
         </Subheading>
       </div>
       <div>
-        <CoursesList courses={SystemDesignCourses} />
+        <CoursesList courses={PythonDivCourses} />
       </div>
     </div>
   );
