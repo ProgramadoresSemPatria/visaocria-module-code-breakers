057c45330b665035ee855f7ada8a28a562f5b54e
Gabriel Melo <bielrj170@gmail.com>
2024-12-11 02:03:01 -0300

    feat: insert onclick open config modal

--- frontend/src/components/molecules/PreModalNavigation/index.tsx
+++ frontend/src/components/molecules/PreModalNavigation/index.tsx
@@ -1,12 +1,21 @@
 import Button from 'src/components/atoms/Button';
+import useRenderRoadmapStore from 'src/store/renderRoadmapStore';
+import useRenderRoadmap from 'src/store/renderRoadmapStore';
 
 const PreModalNavigation = () => {
+  const { setRoadmap } = useRenderRoadmapStore();
   return (
     <div className="flex gap-2 self-center">
-      <Button className="rounded-full text-center px-4 hover:bg-[#1e1e2f] py-1 transition-colors ease-linear transform duration-200 text-[#e0e0e0] text-lg">
+      <Button
+        onClick={() => setRoadmap('Algorithms')}
+        className="rounded-full text-center px-4 hover:bg-[#1e1e2f] py-1 transition-colors ease-linear transform duration-200 text-[#e0e0e0] text-lg"
+      >
         Algorithms
       </Button>
-      <Button className="rounded-full text-center px-4 hover:bg-[#1e1e2f] py-1 transition-colors ease-linear transform duration-200 text-[#e0e0e0] text-lg">
+      <Button
+        onClick={() => setRoadmap('Courses')}
+        className="rounded-full text-center px-4 hover:bg-[#1e1e2f] py-1 transition-colors ease-linear transform duration-200 text-[#e0e0e0] text-lg"
+      >
         Courses
       </Button>
     </div>
